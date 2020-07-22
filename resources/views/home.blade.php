248cdfaf215654541ed8a55c6c57781bfda8ea7e
daniel-lopes <daniellopesdasilva98@gmail.com>
2020-07-22 15:37:02 -0600

    Creating function to change the location of the map with the user's location

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,11 @@
     integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
     crossorigin=""/>
     <style type="text/css">
-       #mapid { height: 180px; }
-   </style>
-   <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-   crossorigin=""></script>
+     #mapid { height: 180px; }
+ </style>
+ <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+ integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+ crossorigin=""></script>
 </head>
 <body>
     <div id="mapid" style="width: 600px; height: 400px;"></div>
@@ -22,36 +22,48 @@
         var latitude = -3.7894256827927744, 
         longitude = -38.569942600679006;
         var mymap = L.map('mapid').setView([latitude, longitude], 12);
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken: 'your.mapbox.access.token'
-        }).addTo(mymap);
 
         (function () {
-          var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-          targetUrl = 'https://middleware.vanguardatech.com/api/search/retorno_posicoes/?format=json'
-          const fetchPromise = fetch (proxyUrl + targetUrl);
-          result = fetchPromise.then (response => { 
-            return response.json (); 
-        }).then (data => {
-         marcaMapa(data);
-     }).catch(e => {
-         alert(
-            "O mapa não será marcado automaticamente\n"+
-            "Erro: API temporariamente indisponivel!"
-            )
-     });
- })();
+         if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+               latitude = position.coords.latitude;
+               longitude = position.coords.longitude;
+               mymap.setView([latitude, longitude], 13)
+           });
+        }   
+    })()
+    
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
 
- function marcaMapa(result) {
+    (function () {
+      var proxyUrl = 'https://cors-anywhere.herokuapp.com/',
+      targetUrl = 'https://middleware.vanguardatech.com/api/search/retorno_posicoes/?format=json'
+      const fetchPromise = fetch (proxyUrl + targetUrl);
+      result = fetchPromise.then (response => { 
+        return response.json (); 
+    }).then (data => {
+       marcaMapa(data);
+   }).catch(e => {
+       alert(
+        "O mapa não será marcado automaticamente\n"+
+        "Erro: API temporariamente indisponivel!"
+        )
+   });
+})();
+
+function marcaMapa(result) {
     for(let coordenadas in result){
         L.marker([result[coordenadas].latitude, result[coordenadas].longitude]).addTo(mymap)
     }
 }
+
 </script> 
 </body>
 </html>
