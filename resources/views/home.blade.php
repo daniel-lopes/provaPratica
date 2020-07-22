b81979f8240c7277b00312bc88fa0bb6426c4d10
daniel-lopes <daniellopesdasilva98@gmail.com>
2020-07-22 15:20:20 -0600

    trying to consume the API, experiencing error blocked by CORS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,11 +10,11 @@
     integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
     crossorigin=""/>
     <style type="text/css">
-     #mapid { height: 180px; }
-    </style>
- <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
- integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
- crossorigin=""></script>
+       #mapid { height: 180px; }
+   </style>
+   <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+   integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+   crossorigin=""></script>
 </head>
 <body>
     <div id="mapid" style="width: 600px; height: 400px;"></div>
@@ -28,6 +28,21 @@
             zoomOffset: -1,
             accessToken: 'your.mapbox.access.token'
         }).addTo(mymap);
-    </script> 
+
+        (function () {
+          var targetUrl = 'https://middleware.vanguardatech.com/api/search/retorno_posicoes/?format=json'
+          const fetchPromise = fetch (targetUrl);
+          result = fetchPromise.then (response => { 
+            return response.json (); 
+        }).then (data => {
+         console.log(data);
+     }).catch(e => {
+         alert(
+            "O mapa não será marcado automaticamente\n"+
+            "Erro: API temporariamente indisponivel!"
+            )
+     });
+ })();
+</script> 
 </body>
 </html>
